f12b3094198d3700d684dd337a41a825349be9bc
omerolkun <omer.olkun@ug.bilkent.edu.tr>
2021-07-23 02:59:17 +0300

    catalog index page is ok

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,6 +6,11 @@ from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
 admin.site.register(Book)
-admin.site.register(Author)
+#admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(BookInstance)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name','last_name','date_of_birth','date_of_death')
+admin.site.register(Author, AuthorAdmin)
