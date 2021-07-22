4c643b66306465fd74a8d216715a5b5586e9193d
omerolkun <omer.olkun@ug.bilkent.edu.tr>
2021-07-23 02:33:55 +0300

    objects are created for db

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+#name omergenc
+#mail omergenc@gmail.com
+#password Cc717343
 # Register your models here.
+from .models import Author, Genre, Book, BookInstance
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
