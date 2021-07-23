dfa04fb05c6b64863fe43079ba9dc62d5cdf3878
omerolkun <omer.olkun@ug.bilkent.edu.tr>
2021-07-23 03:14:54 +0300

    class based views?

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     """View function for home page of site."""
@@ -23,3 +24,6 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
